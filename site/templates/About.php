745c7a0c1d7cdb9ec3c2fd4324f31da32c60b88d
aidank123 <askelley@smcm.edu>
2022-10-24 19:43:50 -0400

    smaller text about section

--- site/templates/About.php
+++ site/templates/About.php
@@ -12,8 +12,8 @@
           <div class = "about_section"><img class = "about_images" src = <?php echo $p->image()->url() ?> ></div>
 
           <div class = "about_section_text_container">
-            <div class = "about_section"<p class = "about_text"> <?php echo $p -> Paragraph_1() ?> </p></div>
-            <div class = "about_section"<p class = "about_text"> <?php echo $p -> Paragraph_2() ?> </p></div>
+            <div class = "about_section"><p class = "about_text"> <?php echo $p -> Paragraph_1() ?> </p></div>
+            <div class = "about_section"><p class = "about_text"> <?php echo $p -> Paragraph_2() ?> </p></div>
           </div>
         </div>
         <hr>

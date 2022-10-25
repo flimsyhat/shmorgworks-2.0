3255b2799d6b1a97523bacd81add68be02d3fb84
Sean Kelley <seanmkelley11@gmail.com>
2022-10-25 12:14:05 +0200

    Left this off previous commit
    See previous commit about About page

--- site/templates/About.php
+++ site/templates/About.php
@@ -1,28 +1,26 @@
-    <?php snippet('header') ?>
+<?php snippet('header') ?>
 
 
-    <div class = "page">
+<div class = "page">
 
-      <?php foreach ($page->children()->listed() as $p):?>
+  <?php foreach ($page->children() as $p):?>
 
-        <h1 class = "about_headers"> <?php echo $p->title() ?> </h1>
+    <h1 class = "about_headers"> <?php echo $p->title() ?> </h1>
 
-        <div class = "about_section_container">
+    <div class = "about_section_container">
 
-          <div class = "about_section"><img class = "about_images" src = <?php echo $p->image()->url() ?> ></div>
+      <div class = "about_section">
+        <img class = "about_images" src = <?php echo $p->image()->url() ?> >
+      </div>
 
-          <div class = "about_section_text_container">
-            <div class = "about_section"><p class = "about_text"> <?php echo $p -> Paragraph_1() ?> </p></div>
-            <div class = "about_section"><p class = "about_text"> <?php echo $p -> Paragraph_2() ?> </p></div>
-          </div>
-        </div>
-        <hr>
-      <?php endforeach ?>
-
+      <div class = "about_section_text_container">
+          <?php echo $p->text()->kirbytext() ?>
+      </div>
     </div>
+    <hr>
+  <?php endforeach ?>
 
-    <!-- closing div for the .page_columns class -->
-  </div>
+</div>
 
 </body>
 </html>
